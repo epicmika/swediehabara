edba100889b009e87ac02da004e918aad3432235
epicmika <epicmika@users.noreply.github.com>
2018-09-18 15:00:59 +0200

    javascript

--- script.js
+++ script.js
@@ -9,38 +9,66 @@ fetch("product.json")
 })
 
 function createListFromProdData() {
-    var prodSite=document.createElement("div")
+    var prodSite=document.getElementById("produkter")
     prodSite.className="productClass";
     console.log("function");
 
     for(var index=0; index <prodData.length; index++) {
-        var prodCon =createProdCon(prodData[index]);
-        prodSite.appendChild(prodCon);
-        console.log("for");
+        console.log("i loopen");
+
+        var prodCon=createProdCon(prodData[index]);
+        console.log("prodCon")
+        prodSite.innerHTML(prodCon);
+
+        
+        
+
+        //var prodDiv = document.getElementById("produkter")
+        //prodDiv.innerHTML += prodCon;
     }
-    document.body.appendChild(prodSite);
+    
+
+   // document.body.appendChild(prodSite);
 }
 
 function createProdCon(prodData) {
-    var prodCon=document.createElement("div");
-    prodCon.className="prodSiteClass";
-    console.log(prodData)
+
+    console.log("funktion createProdCon");
+    console.log(prodData);
+   // var prodCon=document.createElement("div");
+
+    var prodDiv = document.createElement("div")
+    console.log(prodDiv);
+   // var minaProdukter = "";
+    //prodDiv.innerHTML += minaProdukter;
 
     //Image
-    var getProdImg=document.createElement("img");
-    getProdImg.src="bild/"+prodData.prodImage;
-    prodCon.appendChild(getProdImg);
-    console.log(getProdImg.src)
-
+    // var getProdImg=document.createElement("img");
+    // getProdImg.src="bild/"+prodData.prodImage;
+    // prodDiv.appendChild(getProdImg);
+    // console.log(getProdImg.src)
+    
     //Name
-    var getProdName=document.createElement("p");
-    getProdName.innerText=""+prodData.prodName;
-    prodCon.appendChild(getProdName);
+    var prodName=document.createElement("p");
+    var getProdName=""+prodData.prodName;
+    // console.log(getProdName);
+    console.log("här");
+    //prodName.appendChild(getProdName);
+   
+    //console.log(prodName);
+    //prodDiv.appendChild(getProdName); 
+    console.log("inte här");
+    console.log(getProdName);
 
     //Price
-    var getProdPrice=document.createElement("p");
-    getProdPrice.innerText=""+prodData.prodPrice;
-    prodCon.appendChild(getProdPrice);
+    // var getProdPrice=document.createElement("p");
+    // getProdPrice.innerText=""+prodData.prodPrice;
+    // prodDiv.appendChild(getProdPrice);
 
-    return prodCon;
+    console.log("slutet på funktion");
+
+    console.log(prodCon);
+    console.log(prodDiv);
+
+    return prodDiv;
 }
